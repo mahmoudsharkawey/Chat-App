ffb3a3755372b37123c74218ff144688b6921c8e
Mahmoud Gamal <ma7moudelsharkawey@gmail.com>
2025-07-15 07:04:29 +0300

    test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -57,5 +57,5 @@ app.use("/api/messages", messageRoutes);
 // 🚀 Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
+  console.log(`🚀 Server running on http://localhost:${PORT} 🚀`)
 );
